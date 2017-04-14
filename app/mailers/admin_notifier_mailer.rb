37faaac57516504cffab9a50065acca840712f86
Noah Berman <noah@noahberman.org>
2017-04-14 16:27:42 -0500

    add mailer file

--- app/mailers/admin_notifier_mailer.rb
+++ app/mailers/admin_notifier_mailer.rb
@@ -1,2 +1,10 @@
-class AdminNotifierMailer < ApplicationMailer
+class AdminNotifierMailer < ActionMailer::Base
+  default :from => 'any_from_address@example.com'
+  
+  # send a number to the admin
+  def send_number_to_admin(admin)
+    @admin = admin
+    mail (:to => @admin.email,
+    :subject => 'Ring Ring Ring Ring Ring')
+  end 
 end
